dd97dfc6b4c3eff520c837d95faa15698cb316ff
javier1890 <francisco.uribe@r9.cl>
2020-06-13 23:07:24 -0400

    Cambios finales del proyecto

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function($) {
+
+	  $("a#cio").fancybox({
+				'width'				: '85%',
+				'height'			: '85%'
+			});
+
+
+	   $("a#cto").fancybox({
+				'width'				: '85%',
+				'height'			: '85%',
+			});
+
+	   $("a#cco").fancybox({
+				'width'				: '85%',
+				'height'			: '85%',
+			});
+});
